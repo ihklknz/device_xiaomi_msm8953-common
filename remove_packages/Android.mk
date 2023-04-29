7c96213645213edbef677cd3264a216e1d2d5ba1
hklknz <haikalluthfianinobaluqia598@gmail.com>
2023-04-29 08:39:13 +0000

    msm8953-common: remove_packages: nuke some apps
    Signed-off-by: hklknz <haikalluthfianinobaluqia598@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := Remove_Packages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps GoogleTTS PrebuiltGmail Velvet Drive MarkupGoogle
+LOCAL_OVERRIDES_PACKAGES := Maps GoogleTTS PrebuiltGmail Velvet Drive MarkupGoogle CalculatorGooglePrebuilt talkback CalendarGooglePrebuilt AndroidAutoStubPrebuilt Chrome
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
